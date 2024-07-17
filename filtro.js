d747b792c9c79e7860c7ccd6eb79503281e0b185
Susana Reyes <s.miranda9305@gmail.com>
2024-07-17 17:45:21 -0600

    add homework

--- filtro.js
+++ filtro.js
@@ -8,7 +8,7 @@ const productos = [
   {nombre: "Zapato rojo", tipo: "zapato", color: "rojo", img: "./zapato-rojo.jpg"}
 ]
 
-const li = document.getElementsByName("lista-de-productos")
+const li = document.getElementById("lista-de-productos"); // corregir get element
 const $i = document.querySelector('.input');
 
 for (let i = 0; i < productos.length; i++) {
